f5ebfc4ed4a54c95faeb1a10fa374f46530196b5
Hisyam Harun <norhisyam.harun@bateriku.com>
2026-04-01 05:23:52 +0800

    chore: scaffold Nuxt 4 project

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  future: { compatibilityVersion: 4 },
+
+  modules: [
+    '@onmax/nuxt-better-auth',
+    '@pinia/nuxt',
+  ],
+
+  auth: {
+    clientOnly: true,
+    redirects: {
+      login: '/login',
+      guest: '/panel/dashboard',
+    },
+  },
+
+  routeRules: {
+    '/':                  { redirect: '/login' },
+    '/login':             { auth: 'guest' },
+    '/forgot-password':   { auth: 'guest' },
+    '/reset-password':    { auth: 'guest' },
+    '/setup-password/**': { auth: false },
+    '/panel/**':          { auth: 'user' },
+    '/partner/**':        { auth: 'user' },
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE ?? 'http://localhost:3000',
+    },
+  },
+
+  css: ['~/assets/css/main.css'],
+})
